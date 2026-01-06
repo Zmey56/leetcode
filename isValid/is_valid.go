38185b53b9dc12ada30f6e515dd801d7cc821aa6
AleksTuT <zmey56@gmail.com>
2026-01-06 12:27:02 +0400

    20. Valid Parentheses

---
+++ isValid/is_valid.go
@@ -0,0 +1,38 @@
+package isValid
+
+//20. Valid Parentheses
+
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//An input string is valid if:
+//
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+
+func isValid(s string) bool {
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	pairs := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+
+	stack := []rune{}
+
+	for _, ch := range s {
+		if openBracket, found := pairs[ch]; found {
+			if len(stack) == 0 || stack[len(stack)-1] != openBracket {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, ch)
+		}
+	}
+
+	return len(stack) == 0
+}
