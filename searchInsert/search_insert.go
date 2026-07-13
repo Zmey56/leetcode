a88f21bc2d8d883e56f6f9b799a5916476a0ae0b
AleksTuT <zmey56@gmail.com>
2026-07-13 08:02:09 +0400

    35. Search Insert Position

---
+++ searchInsert/search_insert.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//35. Search Insert Position
+
+// Given a sorted array of distinct integers and a target value,
+// return the index if the target is found. If not, return the index where it would be
+// if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+func searchInsert(nums []int, target int) int {
+    index := 0
+    for _, j := range nums{
+        fmt.Printf("Index %d value %d\n", i, j)
+        if j >= target{
+            return index
+        }
+        index++
+    }
+    return index
+}
